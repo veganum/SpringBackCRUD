eb0ff96b4ae30a5d79a4cecae630c66dc1213e52
José <veganum@hotmail.com>
2020-02-19 16:09:39 +0100

    Creando Controller

---
+++ src/main/java/com/veganum/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -0,0 +1,25 @@
+package com.veganum.springboot.backend.apirest.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.veganum.springboot.backend.apirest.models.entity.Cliente;
+import com.veganum.springboot.backend.apirest.models.services.IClienteService;
+
+@RestController
+@RequestMapping("/api")
+public class ClienteRestController {
+
+	@Autowired
+	private IClienteService clienteService;
+
+	@GetMapping("/clientes")
+	public List<Cliente> index() {
+		return clienteService.findAll();
+	}
+
+}
